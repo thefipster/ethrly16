c44ddb6d76d4a08b1a52f3d14eb4217e2e477a6a
Felix.Reisch <Felix.Reisch@hc-vision.de>
2019-08-22 10:23:17 +0200

    Abstracted tcp connection and packet cache for better testability.

--- src/Toc.EthRly.Core/EthRlyDevice.cs
+++ src/Toc.EthRly.Core/EthRlyDevice.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Runtime.Loader;
-using System.Threading;
 using System.Threading.Tasks;
 using Toc.EthRly.Core.Exceptions;
 using Toc.EthRly.Core.Extensions;
@@ -11,32 +9,20 @@ namespace Toc.EthRly.Core
     public class EthRlyDevice : IDisposable
     {
         private readonly Options _options;
-        private readonly Tranceiver _tranceiver;
-
-        public EthRlyDevice(string hostname)
-            : this(new Options(hostname)) { }
-
-        public EthRlyDevice(string hostname, int port)
-            : this(new Options(hostname, port)) { }
-
-        public EthRlyDevice(string hostname, int port, int timeoutInMs)
-            : this(new Options(hostname, port, TimeSpan.FromMilliseconds(timeoutInMs))) { }
-
-        public EthRlyDevice(string hostname, int port, int timeoutInMs, CancellationToken token)
-            : this(new Options(hostname, port, TimeSpan.FromMilliseconds(timeoutInMs)), token) { }
+        private readonly MessageGovernor _tranceiver;
         
         public EthRlyDevice(Options options)
         {
-            var token = getCancellation();
             _options = options;
-            _tranceiver = new Tranceiver(_options, token);
+            _tranceiver = new MessageGovernor(
+                new PacketCache(options),
+                new TcpTransceiver(options),
+                _options);
         }
 
-        public EthRlyDevice(Options options, CancellationToken token)
-        {
-            _options = options;
-            _tranceiver = new Tranceiver(_options, token);
-        }
+        public async Task<bool[]> GetRelaysStatesAsync() => (await _tranceiver
+            .TransceiveAsync(Commands.GetRelayStates))
+            .ToStateArray(_options.RelayCount);
 
         public async Task<bool> GetRelayStateAsync(int relayIndex)
         {
@@ -45,74 +31,49 @@ namespace Toc.EthRly.Core
             return states[relayIndex];
         }
 
-        public async Task<bool[]> GetRelaysStatesAsync()
-        {
-            var response = await tranceive(Commands.GetRelayStates);
-            return response.ToStateArray(_options.RelayCount);
-        }
-
-        public async Task SetRelayStateAsync(int relayIndex, bool newState)
+        public void SetRelayState(int relayIndex, bool newState)
         {
             throwExceptionOnBadIndex(relayIndex);
             var packet = newState
                 ? new Packet(Commands.RelayOn.AsByte() + relayIndex)
                 : new Packet(Commands.RelayOff.AsByte() + relayIndex);
 
-            await _tranceiver.TranceiveAsync(packet);
+            _tranceiver.Transmit(packet);
         }
 
-        public async Task SetAllRelaysAsync(bool newState)
+        public void SetAllRelays(bool newState)
         {
             var packet = newState
                 ? new Packet(Commands.AllRelaysOn)
                 : new Packet(Commands.AllRelaysOff);
 
-            await _tranceiver.TranceiveAsync(packet);
+            _tranceiver.Transmit(packet);
         }
 
         public async Task<bool> ToggleRelayStateAsync(int relayIndex)
         {
             throwExceptionOnBadIndex(relayIndex);
             var state = await GetRelayStateAsync(relayIndex);
-            await SetRelayStateAsync(relayIndex, !state);
+            SetRelayState(relayIndex, !state);
             return !state;
         }
 
-        public async Task<double> GetVoltageAsync()
-        {
-            var response = await tranceive(Commands.GetInputVoltage);
-            return response.ToVoltage();
-        }
+        public async Task<double> GetVoltageAsync() => (await _tranceiver
+            .TransceiveAsync(Commands.GetInputVoltage))
+            .ToVoltage();
 
-        public async Task<int> GetFirmwareVersionAsync()
-        {
-            var response = await tranceive(Commands.GetFirmwareVersion);
-            return response.ToFirmwareVersion();
-        }
+        public async Task<int> GetFirmwareVersionAsync() =>  (await _tranceiver
+            .TransceiveAsync(Commands.GetFirmwareVersion))
+            .ToFirmwareVersion();
 
-        public async Task<byte[]> GetMacAddressAsync()
-        {
-            var macAddress = await tranceive(Commands.GetMacAddress);
-            return macAddress.Payload;
-        }
-
-        private async Task<Packet> tranceive(Commands command)
-        {
-            var request = new Packet(Commands.GetFirmwareVersion);
-            return await _tranceiver.TranceiveAsync(request);
-        }
+        public async Task<byte[]> GetMacAddressAsync() =>  (await _tranceiver
+            .TransceiveAsync(Commands.GetMacAddress))
+            .Payload;
 
         private void throwExceptionOnBadIndex(int relayIndex)
         {
             if (relayIndex < 0 || relayIndex >= _options.RelayCount)
                 throw new InvalidRelayAddressException(relayIndex);
-        }
-
-        private CancellationToken getCancellation()
-        {
-            var tokenSource = new CancellationTokenSource();
-            AssemblyLoadContext.Default.Unloading += context => tokenSource.Cancel();
-            return tokenSource.Token;
         }
 
         public void Dispose()
---
+++ src/Toc.EthRly.Core/Interface/ICache.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Toc.EthRly.Core.Interface
+{
+    public interface ICache : IDisposable
+    {
+        void Enqueue(Packet packet);
+        Task<Packet> DequeueAsync();
+        Task EnqueueAsync(Packet request, Packet response);
+        Task<Packet> DequeueAsync(Packet request);
+    }
+}
